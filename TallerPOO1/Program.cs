3e6373cd515228e55dd564f9e1ab2f40e6f6fe23
Velastorium <velacastanos@gmail.com>
2024-03-18 17:21:04 -0500

    Agregar archivos de proyecto.

---
+++ TallerPOO1/Program.cs
@@ -0,0 +1,67 @@
+﻿class Cuenta
+{
+    public string nombre;
+    public int saldo;
+    public Cuenta(int saldoInicial)
+    {
+        if (saldoInicial >= 0)
+        {
+            saldo = saldoInicial;
+        }
+        else
+        {
+            saldo = 0;
+            Console.WriteLine("Error: El saldo inicial es inválido. Se estableció el saldo en 0.");
+        }
+    }
+
+    public void Credit(int monto)
+    {
+        saldo += monto;
+    }
+
+    public void cargar(int monto)
+    {
+        if (monto <= saldo)
+        {
+            saldo -= monto;
+        }
+        else
+        {
+            Console.WriteLine("El monto a cargar excede el saldo de la cuenta.");
+        }
+    }
+
+    public void ObtenerSaldo()
+    {
+        Console.WriteLine("Su saldo es " + saldo);
+    }
+    static void Main()
+    {
+        Cuenta cuenta1 = new Cuenta(100);
+        cuenta1.Credit(50);
+        cuenta1.cargar(25);
+        cuenta1.ObtenerSaldo();
+
+        Cuenta cuenta2 = new Cuenta(250);
+        cuenta2.Credit(75);
+        cuenta2.cargar(400);
+        cuenta2.ObtenerSaldo();
+    }
+}
+/*1. Cree una clase llamada Cuenta que podría ser utilizada por un banco para
+representar las cuentas bancarias de sus clientes. Incluya un miembro de datos de
+tipo int para representar el saldo de la cuenta.
+a. Proporcione un constructor que reciba un saldo inicial y lo utilice para inicializar
+el miembro de dato. El constructor debe validar el saldo inicial para asegurar
+que sea mayor o igual que 0. De no ser así, establezca el saldo en 0 y muestre
+un mensaje de error, indicando que el saldo inicial era inválido.
+b. Proporcione tres funciones miembros.
+i. La función miembro credit debe agregar un monto al saldo actual.
+ii. La función miembro cargar deberá retirar dinero el objeto Cuenta y
+asegurarse que el monto a cargar no exceda el saldo de Cuenta. Si lo
+hace, el saldo deberá permanecer sin cambio y la función deberá imprimir
+un mensaje que indique “El monto a cargar excede el saldo de la cuenta”.
+iii. La función miembro obtenerSaldo debe devolver el saldo actual.
+c. Cree un programa que cree dos objetos Cuenta y evalué las funciones
+miembros de la clase Cuenta.*/
---
+++ TallerPOO1/Punto2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+class Factura
+{
+    private string numpieza;
+    private string descripcion;
+    private int cantidad;
+    private int precio;
+    private Factura(string numpieza, string descripcion, int cantidad, int precio)
+    {
+        this.numpieza = numpieza;
+        this.descripcion = descripcion;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+    public void EstablecerNumPieza(string numPieza)
+    {
+        this.numpieza = numPieza;
+    }
+
+    public string ObtenerNumPieza()
+    {
+        return numpieza;
+    }
+    public void EstablecerDescripcion(string descripcion)
+    {
+        this.descripcion = descripcion;
+    }
+
+    public string ObtenerDescripcion()
+    {
+        return descripcion;
+    }
+    public void EstablecerCantidad(int cantidad)
+    {
+        this.cantidad = cantidad;
+    }
+
+    public int ObtenerCantidad()
+    {
+        return cantidad;
+    }
+
+    public void EstablecerPrecio(int precio)
+    {
+        this.precio = precio;
+    }
+
+    public int ObtenerPrecio()
+    {
+        return precio;
+    }
+
+    public int obtenerMontoFactura()
+    {
+        if (cantidad < 0)
+        {
+            cantidad = 0;
+        }
+        if (precio < 0)
+        {
+            precio = 0;
+        }
+        return this.cantidad * this.precio;
+    }
+
+    static void Main()
+    {
+        Factura factura1 = new Factura("NA21", "Silla", 45, -100000);
+        Console.WriteLine("El valor a pagar es de: " + factura1.obtenerMontoFactura());
+    }
+}
+/*Cree una clase llamada Factura, que una ferretería podría utilizar para
+representar una factura por un artículo vendido en la tienda. Una factura debe incluir
+cuatro piezas de información como miembros de datos: un número de pieza (string)
+la descripción de la pieza (string), la cantidad
+de artículos de ese tipo que se van a comprar (int) y el precio por artículo (int).
+a. Su clase debe tener un método constructor que inicialice a los cuatro
+miembros de datos.
+b. Proporcione una función establecer y una función obtener para cada
+miembro de datos.
+c. Proporcione una función miembro llamada obtenerMontoFactura que calcule
+el monto de la factura (es decir, que multiplique la cantidad por el precio del
+articulo) y después devuelva ese monto como un valor int.
+d. Si la cantidad no es positiva, debe establecerse en 0. 
+Si el precio por artículo
+no es positivo, debe establecerse en 0.
+e. Escriba un programa de prueba que demuestra las capacidades de la clase
+Factura.*/
